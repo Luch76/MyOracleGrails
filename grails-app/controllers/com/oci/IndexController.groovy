f1ecf5353a3fda2d4b9b173ce76eccc79cc54b6e
Luciano Belotto <DarthLuch@gmail.com>
2024-09-18 10:43:40 -0400

    2024-09-17 - Index controller

--- grails-app/controllers/com/oci/IndexController.groovy
+++ grails-app/controllers/com/oci/IndexController.groovy
@@ -1,9 +1,28 @@
 package com.oci
 
+import groovy.sql.Sql
+
+import javax.sql.DataSource
+import java.sql.Timestamp
+
+import static org.springframework.http.HttpStatus.OK
+
 class IndexController {
 
+    DataSource dataSource;
+
     def index() {
-        render "OK";
+        Sql sql = new Sql(dataSource);
+        Map retVal = [:];
+        Timestamp timestamp;
+
+        sql.eachRow("SELECT SYSDATE FROM DUAL") {it ->
+            retVal.databaseTime = it."SYSDATE";
+        }
+        sql.close();
+
+        respond ([retVal: retVal]);
+
     }
 
 }
---
+++ grails-app/views/index/index.gsp
@@ -0,0 +1,8 @@
+<html>
+
+</html>
+
+<body>
+Database time: ${retVal.databaseTime}
+</body>
+
